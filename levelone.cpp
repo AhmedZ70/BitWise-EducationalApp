ba72c7f93fcf2fa48fc10e9b04424d41290c5aef
JRonca11 <136387195+JRonca11@users.noreply.github.com>
2024-04-17 14:32:17 -0600

    added backend for first level

--- levelone.cpp
+++ levelone.cpp
@@ -1,2 +1,20 @@
 #include "levelone.h"
 
+LevelOne::LevelOne() : inputA(false), inputB(false), result(false) {}
+
+void LevelOne::setInput(bool a, bool b) {
+    inputA = a;
+    inputB = b;
+}
+
+void LevelOne::computeOutput() {
+
+    andGate.setInput(inputA, inputB);
+
+    result = andGate.computeOutput();
+}
+
+bool LevelOne::getResult() const {
+    return result;
+}
+
--- levelone.h
+++ levelone.h
@@ -1,9 +1,22 @@
+// LevelOne.h
 #ifndef LEVELONE_H
 #define LEVELONE_H
 
-#include "logicgate.h"
+#include "andGate.h"
 
 class LevelOne {
+private:
+    AndGate andGate; // Used for calculating the carry
 
+    bool inputA;
+    bool inputB;
+    bool result;
+
+public:
+    LevelOne();
+    void setInput(bool a, bool b);
+    void computeOutput();
+    bool getResult() const;
 };
+
 #endif // LEVELONE_H
