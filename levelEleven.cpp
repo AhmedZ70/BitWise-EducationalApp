6bc3feeda0062f0931d49df5584ad3dcfa9a79b9
joecorbeil <josephcorbeil2@gmail.com>
2024-04-22 11:23:18 -0600

    Level eleven backend implemented

--- levelEleven.cpp
+++ levelEleven.cpp
@@ -1,3 +1,50 @@
 #include "levelEleven.h"
 
-LevelEleven::LevelEleven() {}
+LevelEleven::LevelEleven() : firstBit(false), secondBit(false), thirdBit(false),
+    fourthBit(false), fifthBit(false), sixthBit(false) {}
+
+void LevelEleven::setInput(std:: vector<bool> inputs) {
+    firstBit = inputs.at(0);
+    secondBit = inputs.at(1);
+    thirdBit = inputs.at(2);
+    fourthBit = inputs.at(3);
+    fifthBit = inputs.at(4);
+    sixthBit = inputs.at(5);
+}
+
+void LevelEleven::computeOutput() {
+    Gate gate1 = setGate(userGateSelected1);
+    Gate gate2 = setGate(userGateSelected2);
+    Gate gate3 = setGate(userGateSelected3);
+    Gate gate4 = setGate(userGateSelected4);
+    Gate gate5 = setGate(userGateSelected5);
+
+    gate1.setInput(firstBit, secondBit);
+    bool firstResult = gate1.computeOutput();
+
+    gate2.setInput(thirdBit, fourthBit);
+    bool secondResult = gate2.computeOutput();
+
+    gate3.setInput(fifthBit, sixthBit);
+    bool thirdResult = gate3.computeOutput();
+
+    gate4.setInput(secondResult, firstResult);
+    bool fourthResult = gate4.computeOutput();
+
+    gate5.setInput(thirdResult, secondResult);
+    bool fifthResult = gate5.computeOutput();
+
+    result = fourthResult;
+    //result2 = fifthResult;
+}
+
+void LevelEleven::setGateTypes(const std::vector<std::string>& gateTypes){
+    userGateSelected1 = gateTypes.at(0);
+    userGateSelected2 = gateTypes.at(1);
+    userGateSelected3 = gateTypes.at(2);
+    userGateSelected4 = gateTypes.at(3);
+    userGateSelected5 = gateTypes.at(4);
+}
+
+
+
--- levelEleven.h
+++ levelEleven.h
@@ -3,8 +3,27 @@
 #include "circuitLevel.h"
 class LevelEleven : public CircuitLevel
 {
+private:
+    bool firstBit;
+    bool secondBit;
+    bool thirdBit;
+    bool fourthBit;
+    bool fifthBit;
+    bool sixthBit;
+    string userGateSelected1;
+    string userGateSelected2;
+    string userGateSelected3;
+    string userGateSelected4;
+    string userGateSelected5;
+
 public:
     LevelEleven();
+
+    void setInput(std::vector<bool> inputs);
+
+    void setGateTypes(const std::vector<std::string>& gateTypes);
+
+    void computeOutput();
 };
 
 #endif // LEVELELEVEN_H
