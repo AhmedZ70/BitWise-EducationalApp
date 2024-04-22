94fda73fceca3e8dba91dc2f705ccb2c5ee42c98
joecorbeil <josephcorbeil2@gmail.com>
2024-04-22 14:10:50 -0600

    level 5-8 backend commenting added

--- levelSeven.cpp
+++ levelSeven.cpp
@@ -1,26 +1,38 @@
 #include "levelSeven.h"
 #include "gate.h"
+
+/**
+ * @author Joseph Corbeil, Johnny Song, Ezekiel Jaramillo, Ahmed Zahran, Raj Reddy, Joel Ronca
+ * @date April. 22, 2024
+ * @name levelSeven cpp file for assignment9
+ * This cpp file contains the the implementation of the methods outlines in the levelSeven h file.
+*/
 LevelSeven::LevelSeven() : firstBit(false), secondBit(false), thirdBit(false) {}
 
 void LevelSeven::setInput(std:: vector<bool> inputs) {
+    // Sets the user input
     firstBit = inputs.at(0);
     secondBit = inputs.at(1);
     thirdBit = inputs.at(2);
 }
 
 void LevelSeven::computeOutput() {
+    // Create gate objects for the gates on the circuit
     Gate firstGate = setGate(userGateSelected1);
     Gate secondGate = setGate(userGateSelected2);
 
+    // Sets gate and gets inputs from user while calculating outputs of the gates
     firstGate.setInput(firstBit, secondBit);
     bool firstResult = firstGate.computeOutput();
     secondGate.setInput(firstResult, thirdBit);
     bool finalResult = secondGate.computeOutput();
-    result = finalResult;
 
+    // Final output of the circuit
+    result = finalResult;
 }
 
 void LevelSeven::setGateTypes(const std::vector<std::string>& gateTypes){
+    // Sets the user gate type drag and drop assignment
     userGateSelected1 = gateTypes.at(0);
     userGateSelected2 = gateTypes.at(1);
 
--- levelSix.cpp
+++ levelSix.cpp
@@ -1,21 +1,31 @@
 #include "levelSix.h"
 #include <iostream>
+
+/**
+ * @author Joseph Corbeil, Johnny Song, Ezekiel Jaramillo, Ahmed Zahran, Raj Reddy, Joel Ronca
+ * @date April. 22, 2024
+ * @name levelSix cpp file for assignment9
+ * This cpp file contains the the implementation of the methods outlines in the levelSix h file.
+*/
 LevelSix::LevelSix() : inputA(false), inputB(false){}
 
 void LevelSix::setInput(std:: vector<bool> inputs) {
+    // Sets the user input
     inputA = inputs.at(0);
     inputB = inputs.at(1);
 }
 
 void LevelSix::computeOutput() {
+    // Sets gate and gets inputs from user while calculating outputs of the gates
     Gate andGate = setGate("AND");
     andGate.setInput(inputA, inputB);
     bool firstResult = andGate.computeOutput();
     Gate notGate = setGate("NOT");
     notGate.setInput(firstResult);
     bool finalResult = notGate.computeOutput();
-    result = finalResult;
 
+    // Final output of the circuit
+    result = finalResult;
 }
 
 
