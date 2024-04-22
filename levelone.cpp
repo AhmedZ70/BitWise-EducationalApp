fb6f0bb9e1cd6db3e8b660226e3afdb61b102bb3
joecorbeil <josephcorbeil2@gmail.com>
2024-04-22 11:49:32 -0600

    Level one backend comments added

--- levelone.cpp
+++ levelone.cpp
@@ -1,6 +1,13 @@
 #include "levelone.h"
 #include "gate.h"
 
+/**
+ * @author Joseph Corbeil, Johnny Song, Ezekiel Jaramillo, Ahmed Zahran, Raj Reddy, Joel Ronca
+ * @date April. 22, 2024
+ * @name levelOne cpp file for assignment9
+ * This cpp file contains the the implementation of the methods outlines in the levelOne h file.
+*/
+
 LevelOne::LevelOne() : inputA(false), inputB(false)
 {
     expectedGate = "AND";
@@ -12,8 +19,11 @@ void LevelOne::setInput(std:: vector<bool> inputs) {
 }
 
 void LevelOne::computeOutput() {
+    // Sets gate and gets inputs from user
     Gate andGate = setGate("AND");
     andGate.setInput(inputA, inputB);
+
+    // Computes circuit output and sets circuit model result
     result = andGate.computeOutput();
 }
 
