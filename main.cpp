0ed0338e65858f30cb9cd21cb5dfd46aae9e105f
joecorbeil <josephcorbeil2@gmail.com>
2024-04-23 12:46:09 -0600

    main comments added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 
 #include <QApplication>
 
+/**
+ * @author Joseph Corbeil, Johnny Song, Ezekiel Jaramillo, Ahmed Zahran, Raj Reddy, Joel Ronca
+ * @date April 22, 2024
+ * @name Main cpp file for assignment9
+ * This cpp file launches are QApplication starting our program.
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
